0f29034190c7197a9c981e2d38a7afad5815699e
Alex <sashaL73@gmail.com>
2025-09-19 21:32:50 +0300

    Добавлен цикл с зайцами

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет заяц");
         System.out.println("Привет заяц");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Заяц " + i);
+
+        }
     }
 }
